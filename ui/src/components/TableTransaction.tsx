79532e4f2a0773b320914aeca12df44ff06a91b9
Donald Ho <dh3coding@gmail.com>
2025-07-23 01:53:30 +1000

    revert accident delete

---
+++ ui/src/components/TableTransaction.tsx
@@ -0,0 +1,8 @@
+export default function TbTransaction(){
+    
+    return (
+        <div>
+            
+        </div>
+    );
+}
---
+++ ui/src/utils/dataInterface.ts
@@ -0,0 +1,79 @@
+// ******************** app context ****************
+export interface AppContextType {
+    isLoggedin: boolean;
+    portList: string[] | undefined;
+    selectedPortfolio: string | undefined;
+    selectedPortPath: string | undefined;
+    cashBalance: number;
+    marginBalance: number;
+    positionValue: number;
+    netWorth: number;
+    cashflowCount: number;
+    transactionCount: number;
+    mtmTime: string | undefined;
+    // Add the function to update selected portfolio
+    updateSelectedPortfolio: (portfolioId: string) => void;
+}
+
+// **************** portfolio ****************
+export interface newportfolio {
+    broker: string,
+    note: string,
+    cash: number,
+    margin: number,
+    position_value: number,
+    net_worth: number,
+    cashflow_count: number,
+    transaction_count: number,
+    mtm_time_stamp: number,
+    created_at: string,
+}
+
+// **************** cashflow ****************
+export interface Cashflow {
+    date: string,
+    type: 'in' | 'out',
+    amount: number,
+    bal_prev: number,
+    bal_after: number,
+    reason: 'cash in' | 'sell' | 'buy' | 'cash out' | 'other',
+    time_stamp: number,
+    note?: string,
+    created_at?: string,
+}
+
+export interface GridCashflowRowEntry {
+    id:string,
+    date: string,
+    type: 'in' | 'out',
+    amount: number,
+    bal_prev: number,
+    bal_after: number,
+    reason: 'cash in' | 'sell' | 'buy' | 'cash out' | 'other',
+    time_stamp: number,
+    note?: string,
+    created_at?: string,
+}
+
+
+// **************** transaction ****************
+export interface Transaction {
+    ticker: string,
+    amount: number,
+    price: number,
+    type: 'buy' | 'sell',
+    time_stamp: number,
+    time: string,
+}
+
+
+// **************** position ****************
+export interface GridPositionRowEntry {
+    id: string;  // Unique identifier for each row -> same as ticker
+    cost: number;
+    amount: number;
+    marketPrice: number;
+    marketValue: number;
+    pnl: number;
+    pnlPct: string; // Percentage formatted as a string
+}
