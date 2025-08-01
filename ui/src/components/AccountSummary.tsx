02735c4387cc2dd2e079094b973eae8eea309220
Donald Ho <dh3coding@gmail.com>
2025-08-01 13:04:45 +1000

    ui: value formating

--- ui/src/components/AccountSummary.tsx
+++ ui/src/components/AccountSummary.tsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { PortfolioContext } from "../utils/contexts";
 import { Grid } from "@mui/material";
+import { valueFormatter2D } from "./ZCommonComponents";
 
 export default function AccountSummary() {
     const portfolioContext = useContext(PortfolioContext);
@@ -21,7 +22,8 @@ export default function AccountSummary() {
                 <Grid size={{ xs: 6, md: 3 }} key={k}>
                     <div className="R1Card">
                         <span className="cardR1text1">{k}</span> <br />
-                        <span className="cardR1text2">${Number(v).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
+                        {/* <span className="cardR1text2">${Number(v).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span> */}
+                        <span className="cardR1text2">${valueFormatter2D(v)}</span>
                     </div>
                 </Grid>
             )}
